f2521e70b738fe86e135c83b08ec5e39c1334bfb
Rahul Madan Raju <38735728+rahulmadanraju@users.noreply.github.com>
2023-01-03 15:35:37 +0000

    added function to generate password

---
+++ main.py
@@ -0,0 +1,40 @@
+""" This file includes functions of the password generation. """
+
+from random import choices
+import string
+from typing import List
+
+specials = "~!@#$%^&*()_-+={[}]|\:<,>.?/"
+
+all_pass_chars = string.ascii_letters + string.digits + specials
+
+
+def password_gen(*, remove: List[str] = None, password_length: int = 8):
+    """
+    This function generates the password when called. It also contains the
+    feature to provide the password containing chars based on user provided
+    length and also to remove/filter certain chars if not required
+    in the password.
+    Args:
+     - remove: filter the not required chars from password
+     - password_length: provide the length of the password
+
+    Returns:
+        password as per the user requirement
+    """
+    assert type(
+        remove) == list or remove is None, f"Not of a required type 'List' " \
+                                           f"instead it is '{type(remove)}'."
+    assert type(
+        password_length) == int, f"Not of a required type 'int' instead it " \
+                                 f"is '{type(remove)}'."
+    if remove:
+        filtered_pass = str([all_pass_chars.replace(i, "") for i in remove])
+        return ''.join(choices(filtered_pass, k=password_length))
+    else:
+        return ''.join(choices(all_pass_chars, k=password_length))
+
+
+if __name__ == "__main__":
+    pass_gen = password_gen()
+    print(f"Password is: {pass_gen}")
