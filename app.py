9e64193ff13f587dc74fc73660e1f307983ada69
Rahul Madan Raju <38735728+rahulmadanraju@users.noreply.github.com>
2023-01-04 14:12:52 +0000

    removed unused imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from main import password_gen
 
 app = Flask(__name__, static_folder="static")
