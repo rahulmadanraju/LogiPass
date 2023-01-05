bf326df590d8c2a294f186b6daa551ca7a355d20
Rahul Madan Raju <rahulmadanraju@gmail.com>
2023-01-06 00:15:29 +0100

    resolve pylint issues

--- logipass/app.py
+++ logipass/app.py
@@ -7,6 +7,12 @@ app = Flask(__name__, static_folder="static")
 
 @app.route("/", methods=["GET", "POST"])
 def my_func():
+    """ Executes the password generator function on clicking
+    the generate button
+
+    Returns:
+        renders template index.html with password
+    """
     if request.method == "POST":
         output = password_gen(password_length=12)
         return render_template("index.html", output=output)
