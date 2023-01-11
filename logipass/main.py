a3ec6e9e89d5b33db6cdc9e02d19acecddebcef5
Rahul Madan Raju <rahulmadanraju@gmail.com>
2023-01-11 22:44:00 +0100

    added code to return password based on arguments

--- logipass/main.py
+++ logipass/main.py
@@ -9,7 +9,9 @@ SPECIALS = "~!@#$%^&*()_-+={[}]|:<,>.?/"
 ALL_PASS_CHARS = string.ascii_letters + string.digits + SPECIALS
 
 
-def password_gen(*, remove: List[str] = None, password_length: int = 8):
+def password_gen(*, letters: bool = False, numbers: bool = False,
+                 special_chars: bool = False,
+                 password_length: int = 8):  # remove: List[str] = None,
     """
     This function generates the password when called. It also contains the
     feature to provide the password containing chars based on user provided
@@ -22,27 +24,69 @@ def password_gen(*, remove: List[str] = None, password_length: int = 8):
     Returns:
         password as per the user requirement
     """
-    assert isinstance(remove, list) or remove is None, (
-        f"Not of a required type 'List' " f"instead it is '{type(remove)}'."
-    )
-    assert isinstance(password_length, int), (
-        f"Not of a required type 'int' instead it " f"is '{type(remove)}'."
-    )
 
-    # check for only letters
-    # check for only numbers
-    # check for only special_chars
-    # check for numbers+letters
-    # check for numbers+chars
-    # check for letters+chars
-    # check for letters+numbers+chars
-    
-    if remove:
+    if letters and numbers and special_chars:
+        password = "".join(choices(ALL_PASS_CHARS, k=password_length))
+
+    elif letters and numbers:
+        password = "".join(
+            choices(string.ascii_letters + string.digits, k=password_length))
+
+    elif numbers and special_chars:
+        password = "".join(
+            choices(string.digits + SPECIALS, k=password_length))
+
+    elif letters and special_chars:
+        password = "".join(
+            choices(string.ascii_letters + SPECIALS, k=password_length))
+
+    elif special_chars:
+        password = "".join(choices(SPECIALS, k=password_length))
+
+    elif numbers:
+        password = "".join(choices(string.digits, k=password_length))
+
+    elif letters:
+        password = "".join(choices(string.ascii_letters, k=password_length))
+
+    else:
+        password = "".join(choices(ALL_PASS_CHARS, k=password_length))
+
+    return password
+    '''assert isinstance(remove, list) or remove is None, (
+            f"Not of a required type 'List' " f"instead it is '{type(
+            remove)}'."
+        )
+        assert isinstance(password_length, int), (
+            f"Not of a required type 'int' instead it " f"is '{type(remove)}'."
+        )'''
+
+    '''if remove:
         filtered_pass = str([ALL_PASS_CHARS.replace(i, "") for i in remove])
         return "".join(choices(filtered_pass, k=password_length))
 
-    return "".join(choices(ALL_PASS_CHARS, k=password_length))
+    return "".join(choices(ALL_PASS_CHARS, k=password_length))'''
 
 
 if __name__ == "__main__":
-    print(f"Password is: {password_gen()}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=True, numbers=True, special_chars=False)}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=True, numbers=False, special_chars=True)}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=False, numbers=True, special_chars=True)}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=False, numbers=False, special_chars=True)}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=False, numbers=True, special_chars=False)}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=True, numbers=False, special_chars=False)}")
+    print(
+        f"Password is: "
+        f"{password_gen(letters=True, numbers=True, special_chars=True)}")
